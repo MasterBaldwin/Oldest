19bbf4bb009e7c98471f1854729c257fbba14193
Mike Baldwin <Michael.Baldwin3@rockets.utoledo.edu>
2018-02-14 18:28:26 -0500

    Working sorting algo and confirmation of null value age.

--- src/Oldest.java
+++ src/Oldest.java
@@ -10,24 +10,17 @@ public static void main(String[] args) {
     JTextField nameField = new JTextField(15);
     JTextField ageField = new JTextField(3);
     int confirmCode;
-    String name = "";
 
     panel.add(new JLabel("Name: ")); panel.add(nameField);
     panel.add(Box.createHorizontalStrut(15));
     panel.add(new JLabel("Age: ")); panel.add(ageField);
 
-    while(!name.equals("quit"))
-    {
-        confirmCode = JOptionPane.showConfirmDialog(null, panel,
-                "Finding the Oldest", JOptionPane.DEFAULT_OPTION);
-
-        if(confirmCode == JOptionPane.OK_OPTION)
-        {
-            members.add(new Person(nameField.getText(), Integer.parseInt(ageField.getText())));
-            name = nameField.getText();
-            nameField.setText("");
-            ageField.setText("");
-        }
+    while (!nameField.getText().equals("quit")) {
+        nameField.setText("");
+        ageField.setText("");
+        confirmCode = JOptionPane.showConfirmDialog(null, panel, "Finding the Oldest", JOptionPane.DEFAULT_OPTION);
+        if (confirmCode == JOptionPane.OK_OPTION)
+            members.add(new Person(nameField.getText(), ageField.getText().isEmpty() ? 0 : Integer.parseInt(ageField.getText())));
     }
 
     members.sort(Comparator.comparingInt(o -> o.age));
